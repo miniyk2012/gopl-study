80099ca38f7f9dfde5191808df9f8b2de9c161f9
杨恺 <yangkai02@qutoutiao.net>
2020-04-22 15:39:00 +0800

    [feature]map

--- go-core36/article9/exercise/xx.go
+++ go-core36/article9/exercise/xx.go
@@ -12,9 +12,10 @@ type Bd struct {
 
 func main() {
 	a := "abc"
+	b := "abc"
 	ad := Ad{&a}
-	bd := Bd{a}  // 做了复制, 因此不是原来的a咯
+	bd := Bd{a} // 做了复制, 因此不是原来的a咯
 	fmt.Println(ad.AppName == &bd.AppName)
 	fmt.Println(*(ad.AppName) == bd.AppName)
-	fmt.Printf("%v\n%v\n%v\n", &a, ad.AppName, &bd.AppName)
+	fmt.Printf("%v\n%v\n%v\n%v\n", &a, &b, ad.AppName, &bd.AppName)
 }
