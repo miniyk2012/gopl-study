2eb179d024ab62963c4db315a3367f90ad3c9f9b
杨恺 <yangkai02@qutoutiao.net>
2020-05-02 17:26:28 +0800

    20200502

--- ch4/exercise/ex4_2/main.go
+++ ch4/exercise/ex4_2/main.go
@@ -1,7 +1,36 @@
 package main
 
-
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
 func main() {
-
+	var sumType = flag.String("checkSum", "SHA256", "加密")
+	b, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(b))
+	flag.Parse()
+	var c []byte
+	switch *sumType {
+	case "SHA256":
+		temp := sha256.Sum256(b)
+		c = temp[:]
+	case "SHA384":
+		temp := sha512.Sum384(b)
+		c = temp[:]
+	case "SHA512":
+		temp := sha512.Sum512(b)
+		c = temp[:]
+	default:
+		log.Fatal("Unexpected width specified.")
+	}
+	fmt.Printf("%x\n", c)
 }
--- ch4/sha256/main.go
+++ ch4/sha256/main.go
@@ -17,10 +17,13 @@ func zero(ptr *[32]byte) {
 func main() {
 	c1 := sha256.Sum256([]byte("x"))
 	c2 := sha256.Sum256([]byte("X"))
+	c3 := sha256.Sum256([]byte("x\n"))
 	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
+	fmt.Printf("%x\n", c3)
 	// Output:
 	// 2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881
 	// 4b68ab3847feda7d6c62c1fbcbeebfa35eab7351ed5e78f4ddadea5df64b8015
+	// 73cb3858a687a8494ca3323053016282f3dad39d42cf62ca4e79dda2aac7d9ac
 	// false
 	// [32]uint8
 	fmt.Printf("%x\n", c1)
