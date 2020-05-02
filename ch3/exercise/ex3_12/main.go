de129a6abd428947879a487e6dc1194a5f94473c
杨恺 <yangkai02@qutoutiao.net>
2020-05-02 12:29:56 +0800

    20200502

---
+++ ch3/exercise/ex3_12/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func SameChars(s1, s2 string) bool {
+	r1, r2 := []rune(s1), []rune(s2)
+	if len(r1) != len(r2) {
+		return false
+	}
+	for _, r := range r1 {
+		var flag bool
+		for _, rr := range r2 {
+			if r == rr {
+				flag = true
+				break
+			}
+		}
+		if !flag {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(SameChars("世界", "世"))
+	fmt.Println(SameChars("世界", "世界"))
+	fmt.Println(SameChars("a世界", "世a界"))
+	fmt.Println(SameChars("a世界", "世a界世"))
+	fmt.Println(SameChars("hello, 世界", "世he,界 llo"))
+}
